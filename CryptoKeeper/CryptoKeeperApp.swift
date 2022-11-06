3242f18bf4b2426553824fbc072e8e596c6c8f56
Daulet Ashikbayev <dxulet@gmail.com>
2022-11-06 19:32:25 +0600

    Initial Commit

---
+++ CryptoKeeper/CryptoKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoKeeperApp.swift
+//  CryptoKeeper
+//
+//  Created by Daulet Ashikbayev on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
